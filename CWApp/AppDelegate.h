dbd9750827128141602b4ff6d3c5fdac2580b6f9
王朝文 <wangcwvip@163.com>
2018-01-18 17:05:30 +0800

    init project

---
+++ CWApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CWApp
+//
+//  Created by wangcw on 2018/1/18.
+//  Copyright © 2018年 wangcw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CWApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CWApp
+//
+//  Created by wangcw on 2018/1/18.
+//  Copyright © 2018年 wangcw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
